12588a51f24ec6857384d053953a32d1dbdca0c2
DarenMcCune <darenmccune@gmail.com>
2019-01-08 21:56:35 -0800

    Converted the clusters array to a bit array and refactored algorithm to compensate, along with a couple of basic bug fixes

--- data_handoff.py
+++ data_handoff.py
@@ -3,25 +3,34 @@ import sample_data_set as sample
 import numpy as np
 
 def handoff(k):
-    init_centers=[np.array([50, 175], dtype="float32"), np.array([20, 10], dtype="float32")] #Centers are in the same form as the points, i.e. the first array is the x's and the second is the y's
-    x=np.array(sample.data['x'])
-    y=np.array(sample.data['y'])
+    init_centers=[np.array([50, 17], dtype="float32"), np.array([20, 10], dtype="float32")] #Centers are in the same form as the points, i.e. the first array is the x's and the second is the y's
+    x=np.array(sample.data['x'], dtype="float32")
+    y=np.array(sample.data['y'], dtype="float32")
     
     data_size=len(x)
-
-    clusters=[np.array([np.zeroes(data_size),np.zeroes(data_size)], dtype="float32"),np.array([np.zeros(data_size),np.zeros(data_size)], dtype="float32")]
     
-    print init_centers
+    #Declare it as a int8 because np.bool is one byte anyway so there's no difference in size, and it's easier to handoff to C in int8 form 
+    clusters=[np.zeros(data_size, dtype=np.int8) for i in xrange(k)]
+    
+    
     centers_handlers=[np.ctypeslib.as_ctypes(array) for array in init_centers]
     
     centers_pointer=(ctypes.POINTER(ctypes.c_float)*k)(*centers_handlers)
     
-    dataset_handlers=[np.ctypeslib.as_ctypes(x), np.ctypes.as_ctypes(y)]
+    dataset_handlers=[np.ctypeslib.as_ctypes(x), np.ctypeslib.as_ctypes(y)]
     
-    dataset_pointers=(ctypes.POINTER(ctypes/c_float)*2)(dataset_handlers)
+    dataset_pointer=(ctypes.POINTER(ctypes.c_float)*2)(*dataset_handlers)
 
-    #TODO figure out how to handle clusters handler
+    clusters_handlers=[np.ctypeslib.as_ctypes(cluster) for cluster in clusters]
 
-    #ctypes.CDLL("./k_means.so").tester(centers_pointer_array)
-    #ctypes.CDLL("./k_means.so").k_means(ctypes.c_int(2), centers_pointer, dataset_pointer, , ctypes.c_int(x.size))
+    clusters_pointer=(ctypes.POINTER(ctypes.c_int8)*k)(*clusters_handlers)
+    
+    #distance=0
 
+    #distance_pointer=(ctypes.POINTER(ctypes.c_float))(distance)
+
+    
+    cdll=ctypes.CDLL("./k_means.so")
+    distance= cdll.k_means(ctypes.c_int(2), centers_pointer, dataset_pointer, clusters_pointer, ctypes.c_int(x.size)) 
+    print distance, clusters
+    
--- k_means.c
+++ k_means.c
@@ -7,7 +7,7 @@ int distance(double p1[], double p2[]){
 	return pow(p1[0]-p2[0], 2)+pow(p1[1]-p2[1], 2);
 }
 
-double k_means(int k, double** centers, double** dataset,  double*** clusters, int maxindex){
+double k_means(int k, double** centers, double** dataset,  __int8_t** clusters, int maxindex){
 	double point[2], center[2], minDistance, minDistanceCenter, totalDistance, curDistance;
 	int pointIndex, centerIndex, minDistanceIndex, i;
 	for(pointIndex=0; pointIndex<maxindex; pointIndex++){
@@ -24,30 +24,16 @@ double k_means(int k, double** centers, double** dataset,  double*** clusters, i
 			}
 		}
 		totalDistance+=minDistance;
+		//TODO Merge these two loops into one
 		for(i=0; i<k; i++){
 			if(i==minDistanceIndex){
-				clusters[i][0][pointIndex]=point[0];
-				clusters[i][1][pointIndex]=point[1];
+				clusters[i][pointIndex]=1;
 			}
 			else{
-				clusters[i][0][pointIndex]=NAN;
-				clusters[i][1][pointIndex]=NAN;
+				clusters[i][pointIndex]=0;
 			}
 		}
 	}
+	printf("Blah is %f\n", totalDistance);
 	return totalDistance;
-}
-
-int tester(float** centers){
-	int i,j;
-	printf("%f\n", centers[0][0]);
-	puts("{");
-	for(i=0;i<2; i++){
-		printf("{");
-		for(j=0; j<2; j++){
-			printf("%f,", centers[i][j]);
-		}
-		printf("},");
-	}
-	printf("\n");
 }
